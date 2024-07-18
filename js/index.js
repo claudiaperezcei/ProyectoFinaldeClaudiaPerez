9b06c160134562f6745a7e4062f66e6ad868c0c3
Claudia Perez <capl9235@alumnos.cei.es>
2024-07-18 23:31:49 +0200

    Nuevas modificaciones en los archivos más comentarios
    Se han modificado la animación del Banner, implementando la función de setInterval.
    
    Se han corregido algunos elementos de de los archivos html y css.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
 // DECLARACIÓN DE VARIABLES
-
 // declaración de variable --> tab y tabContents para dar funcionalidad a las pestañas de la sección de servicios y que estas se activen y muestren su contenido
 const tabs = document.querySelectorAll('.tab')
 const tabContents = document.querySelectorAll('.tab__content')
+// declaración de variable --> btn__swipeUp para dar funcionalidad al botón que llevará al inicio de cada pag. 
+const btnSwipeUp = document.getElementById('btn__swipeUp')
 // declaración de variables --> Para activar/desactivar el menu responsive 
 const menu = document.querySelector('#menu')
 const menuOpen = document.querySelector('#menu__open')
 const menuClose = document.querySelector('#menu__close')
-
 // declaración de variable --> clase appear, dará funcionalidad de que vayan apareciendo los elementos con esa clase, cuando se haga scroll. 
 let appearElement = document.querySelectorAll('.appear')
-
+// Declaración de variable --> banner, para posteriormente realizar las animaciones con intervalos
+let banner = document.querySelector('.banner')
+// Declaración de variable --> layer__overlay, para superponer la capa al banner
+let overlay = document.querySelector('.layer__overlay')
+// se define un array con las rutas de las imágenes para animar la galería del banner
+let bannerGallery = [
+    './media/cultureGirlBanner.webp',
+    './media/landscapesMachupichu.webp',
+    './media/couplesOne.webp',
+    './media/portraitBoy.webp'
+]
+// se inicia el contador a 0 para llevar la cuenta de la imagen actual
+let counter = 0
 // FUNCIONES
 // se hará un recorrido a todos los elementos con la variable tab
 tabs.forEach(tab => {
@@ -24,24 +36,41 @@ tabs.forEach(tab => {
     document.getElementById(target).classList.add('active')
   })
 })
-// Funcionalidad para que cuando se haga click al botón swipeup, la página se desplace hacia arriba. gracias a esto aporta más funcionalidad para el usuario
-document.getElementById('btn__swipeUp').addEventListener('click', function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-})
+// Funcionalidad para que cuando se haga click al botón swipeup, la página se desplace hacia arriba. gracias a esto aporta más funcionalidad para el usuario. Se ha optimizado el código
+btnSwipeUp.addEventListener('click', () => { window.scrollTo({ top: 0, behavior: 'smooth' })})
 // Funcionalidad para activar la visibilidad del menu responsive para los otros dispositivos
-//Escuchador de eventos --> click, cuando escuche el click en el boton menuOpen, llamará la función y añade la clase --> visible, al elemento del menu, para que se muestre el contenido. Se ha optimizado el código
+//através del escuchador de eventos --> click, cuando este escuche el click en el boton menuOpen, llamará la función y añade la clase --> visible, al elemento del menu, para que se muestre el contenido. Se ha optimizado el código
 menuOpen.addEventListener('click', () => { menu.classList.add("visible") })
 //Escuchador de eventos --> click, cuando escuche el click en el boton menuClose, llamará la función y elimina la clase --> visible, al elemento del menu, para que se oculte el contenido. Se ha optimizado el código
 menuClose.addEventListener('click', () => { menu.classList.remove("visible") })
-
-// Funcionalidad para que cuando se haga scroll, los elementos con la case appear, vayan apareciendo y viceversa. 
+// Funcionalidad con el escuchador de eventos para que cuando se haga scroll, los elementos con la clase appear, vayan apareciendo y viceversa. 
 window.addEventListener('scroll', () => {
     appearElement.forEach((appear) => {
+        // Obtiene las coordenadas del elemento en relación con la ventana del navegador
         if (appear.getBoundingClientRect().top < window.innerHeight - 200) {
+            // Si el elemento está dentro del área visible, es visible
             appear.style.opacity = '1'
         }else {
+            // Si el elemento no está dentro del área visible, elimina el estilo en línea
             appear.removeAttribute('style')
         }
     })
 })
-
+// Se establece un intervalo de tiempo que se ejecutará cada 3000 milisegundos (3s)
+setInterval(() => {
+    setTimeout(() => { //Se establece un timeout que se ejecutará después de 100 milisegundos
+        overlay.style.opacity = 0; //se oculta el overlay
+        banner.style.backgroundImage = 'none'; //se elimina la img actual, para poder cargar después la nueva
+    }, 100);
+    setTimeout(() => {
+        counter++; //se incrementa el contador
+        if (counter >= bannerGallery.length) { //se comprueba con una condicional, si el contador ha realizado todo el recorrido del array.
+            counter = 0; //si es así, el contador es 0
+        }        
+        banner.style.backgroundImage = `url(${bannerGallery[counter]})`; //se establece la nueva img de fondo, correspondiente al contador
+        banner.style.backgroundRepeat = 'no-repeat';
+        banner.style.backgroundSize = 'cover';
+        banner.style.backgroundPosition = 'center';
+        overlay.style.opacity = 1; //se visualiza la capa overlay
+    }, 150);
+}, 3000); //tiempo de transición
